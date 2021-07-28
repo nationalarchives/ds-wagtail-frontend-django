ebf932a18fd3e6bffc79ea08693f8847b9830e99
AshTNA <ashleyaburnett@gmail.com>
2021-07-28 14:20:45 +0100

    More amendments for heroku

--- frontend/settings.py
+++ frontend/settings.py
@@ -84,19 +84,17 @@ MIDDLEWARE_CLASSES = ('whitenoise.middleware.WhiteNoiseMiddleware')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 import os
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
-
 STATIC_URL = '/static/'
 
 
-STATICFILES_DIRS = [
-    BASE_DIR / "frontend/static"
-]
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'frontend/static'),
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
