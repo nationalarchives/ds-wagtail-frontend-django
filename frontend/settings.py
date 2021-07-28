2e4898e76d307cbe46ecc5342f815f4295bda42b
AshTNA <ashleyaburnett@gmail.com>
2021-07-28 13:58:44 +0100

    Update setting for heroku

--- frontend/settings.py
+++ frontend/settings.py
@@ -81,6 +81,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+import os
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 
 STATIC_URL = '/static/'
 
@@ -88,3 +95,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "frontend/static"
 ]
+
+try:
+    import django_heroku
+    django_heroku.settings(locals())
+except:
+    pass
